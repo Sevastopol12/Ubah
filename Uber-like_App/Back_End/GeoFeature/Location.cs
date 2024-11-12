1c230595d81628c4ea90023600933c5a437c44be
Sevastopol <141613215+Sevastopol12@users.noreply.github.com>
2024-11-12 10:46:21 +0700

    Add files via upload

---
+++ Uber-like_App/Back_End/GeoFeature/Location.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ubah
+{
+    public class Location
+    {
+
+        // This class should encompasses everything that are related to "user's location"
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public Location(double Latitude, double Longitude)
+        {
+            this.Latitude = Latitude;
+            this.Longitude = Longitude;
+        }
+
+    }
+}
